343b3240d3edd8bd6c58d864f620184686a641b2
Mansuurr <mansuurashid@gmail.com>
2025-12-11 17:00:48 +0500

    1-9 tasks

---
+++ Week_1/kokokokok.py
@@ -0,0 +1,56 @@
+#first = input("Your first name... ")
+#last = input("Your last name...")
+#age = input("How old are you... ")
+#phone = input("Your phone number... ")
+#print("Your full name: ",first, last)
+#print("Your age: ",age)
+#print("Your phone: ",phone)
+#-------------------------------------------------------------------------------
+#zarplata = list(map(int, input().split()))
+#print(max(zarplata) - min(zarplata))
+#-------------------------------------------------------------------------------
+#a = float(input("Enter a number with two decimal places:"))
+#w_n = int(a)
+#f_n = int(round((a - w_n) * 100))
+#new_n = f_n + w_n / 100
+#print(new_n)
+#-------------------------------------------------------------------------------
+#n = int(input())
+#if n >= 1:
+#    print (n * (n + 1) // 2)
+#else:
+#    print ((1 + n) * (1 - n + 1) // 2)
+#--------------------------------------------------------------------------------
+#otvet = int(input("Enter result: "))
+#zagadal = (r - 16) // 10
+#print("Your number: ",x)
+#---------------------------------------------------------------------------------
+#a = float(input("Your first number: "))
+#operation = input("Operation (+ - * /): ")
+#b = float(input("Your second number: "))
+#if operation == "+":
+#    print(a + b)
+#elif operation == "-":
+#    print(a - b)
+#elif operation == "*":
+#    print(a * b)
+#elif operation == "/":
+#    print(a/b)
+#-----------------------------------------------------------------------------------
+#a = float(input("First number: "))
+#operation = input("Operation (+ - * /): ")
+#b = float(input("Your second number: "))
+#if operation == "/":
+#    print(a / b if b != 0 else "Error: division by zero")
+#-------------------------------------------------------------------------------------
+#word = input("Word: ")
+#n = int(input("Number: "))
+#for ch in word:
+#    print(ch * n)
+#-----------------------------------------------------------------------------------------
+#ticket = input().zfill(6)
+
+#first = sum(map(int, ticket[:3]))
+#last = sum(map(int, ticket[3:]))
+
+#print("Lucky" if first == last else "Not lucky")
